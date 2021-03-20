b8866c8fb87f70e1be38dec363f0b7d451b24493
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-03-20 10:30:32 -0400

    El modelo fue creado.

---
+++ Parcial01/Parcial1_JoseRivera/Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_JoseRivera.Models
+{
+    public class Currency
+    {
+        [Key]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Symbol { get; set; }
+    }
+}
---
+++ Parcial01/Parcial1_JoseRivera/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_JoseRivera.Models
+{
+    public class Product
+    {
+        [Key]
+        [Range(1,99999)]
+        public int IdentityID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Description { get; set; }
+        public Currency Price { get; set; }
+    }
+}
