7f9a7e5b778b0dd496050da41bb6309be8a07eac
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-03-20 10:56:17 -0400

    DataContext creado.

---
+++ Parcial01/Parcial1_JoseRivera/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_JoseRivera.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Parcial01/Parcial1_JoseRivera/Models/DbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Parcial1_JoseRivera.Models
+{
+    public class DbContext
+    {
+        private string v;
+
+        public DbContext(string v)
+        {
+            this.v = v;
+        }
+    }
+}
